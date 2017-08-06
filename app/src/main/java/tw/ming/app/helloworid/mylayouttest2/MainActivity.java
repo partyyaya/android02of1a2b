5107ef71204feecd14e7c499b2f4b64a38323d2a
LBJ <LBJ>
2017-08-06 16:42:51 +0800

    20170806

--- app/src/main/java/tw/ming/app/helloworid/mylayouttest2/MainActivity.java
+++ app/src/main/java/tw/ming/app/helloworid/mylayouttest2/MainActivity.java
@@ -16,69 +16,80 @@ public class MainActivity extends AppCompatActivity {
     private String answer;
     private EditText inputEdit;
     private TextView logText;
-    private  boolean isWinner=false;
-    private int counter=0;
+    private boolean isWinner;
+    private int counter;
+    private String[] items = {"2","3","4","5","6","7"};
+    private int dig = 3;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        inputEdit =(EditText)findViewById(R.id.inputText);
-        logText =(TextView)findViewById(R.id.logText);
+
+        inputEdit = (EditText) findViewById(R.id.inputText);
+        logText = (TextView)findViewById(R.id.logText);
         initGame();
     }
-    private  String createAnswer(int a){
-        Set<Integer> set = new HashSet<>();
-        while(set.size()<a){
-            set.add((int)(Math.random()*10));
-        }
-        StringBuilder sb = new StringBuilder();
-        for(Integer d:set){
-            sb.append(d);
-        }
-        return sb.toString();
-    }
-    public String checkAB(String a,String g){
-        int A,B;
-        A=B=0;
-        for(int i=0;i<g.length();i++){
-            if(g.charAt(i)==a.charAt(i)){
-                A++;
-            }else if(a.indexOf(g.charAt(i))>=0){
-                B++;
-            }
-        }
-        return A+"A"+B+"B";
-    }
-    public void doGuess(View view){
-        //TODO doGuess
-        counter++;
-        Log.d("ming","doGuess");
-        String guess = inputEdit.getText().toString();
-        String result = checkAB(answer,guess);
-        logText.append(guess+":"+result+"\n");
-        inputEdit.setText("");//清除log紀錄
-        if(result.equals("3A0B")){
-            isWinner=true;
-            showDialog();
-        }else if(counter==10){
-            isWinner=false;
-            showDialog();
-        }
-    }
+
     private void initGame(){
-        answer = createAnswer(3);
+        answer = createAnswer(dig);
         Log.d("brad", "answer is " + answer);
         isWinner = false;
         counter = 0;
         logText.setText("");
     }
 
+    // Create Answer
+    private String createAnswer(int num){
+        Set<Integer> set = new HashSet<>();
+        while (set.size()<num){
+            set.add((int)(Math.random()*10));
+        }
+        StringBuilder sb = new StringBuilder();
+        for (Integer d : set){
+            sb.append(d);
+        }
+        return sb.toString();
+    }
+
+    private String checkAB(String a, String g){
+        int A, B; A = B = 0;
+        for (int i=0; i<g.length(); i++){
+            if (g.charAt(i) == a.charAt(i)){
+                A++;
+            }else if (a.indexOf(g.charAt(i))>=0){
+                B++;
+            }
+        }
+        return A + "A" + B + "B";
+    }
+
+    public void doGuess(View view){
+        // TODO doGuess
+        counter++;
+        String guess = inputEdit.getText().toString();
+        String result = checkAB(answer, guess);
+        logText.append(counter + ". " + guess + " : " + result + "\n");
+        inputEdit.setText("");
+
+        if (result.equals(dig + "A0B")){
+            // Winner
+            isWinner = true;
+            showDialog();
+        }else if (counter == 10){
+            // Loser
+            isWinner = false;
+            showDialog();
+        }
+
+    }
+
     private void showDialog(){
         AlertDialog dialog = null;
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle("Result");
-        builder.setMessage(isWinner?"Winner":"Loser"+answer);
-        //builder.setCancelable(false);//不能按其他地方取消
+        builder.setMessage(isWinner?"Winner":"Loser:" + answer);
+        builder.setCancelable(false);
         builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
@@ -87,17 +98,72 @@ public class MainActivity extends AppCompatActivity {
         });
         dialog = builder.create();
         dialog.show();
+
     }
+
+
     public void doReset(View view){
-        //TODO doReset
-        Log.d("ming","doReset");
+        AlertDialog dialog = null;
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Reset?");
+        builder.setMessage("Yes or No");
+        builder.setCancelable(false);
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                initGame();
+            }
+        });
+        builder.setNegativeButton("No", null);
+        dialog = builder.create();
+        dialog.show();
+
     }
     public void doSetting(View view){
-        //TODO doSetting
-        Log.d("ming","doSetting");
+        AlertDialog dialog = null;
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Exit?");
+        builder.setCancelable(false);
+//        builder.setItems(items, new DialogInterface.OnClickListener() {
+//            @Override
+//            public void onClick(DialogInterface dialogInterface, int i) {
+//                dig = i + 2;
+//                initGame();
+//            }
+//        });
+
+        builder.setSingleChoiceItems(items, dig - 2, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dig = i + 2;
+            }
+        });
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                initGame();
+            }
+        });
+
+
+        dialog = builder.create();
+        dialog.show();
     }
     public void doExit(View view){
-        //TODO doExit
-        Log.d("ming","doExit");
+        AlertDialog dialog = null;
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Exit?");
+        builder.setMessage("Yes or No");
+        builder.setCancelable(false);
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                finish();
+            }
+        });
+        builder.setNegativeButton("No", null);
+        dialog = builder.create();
+        dialog.show();
     }
 }
